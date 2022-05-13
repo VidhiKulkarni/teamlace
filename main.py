8415e56a49b5e6a0c2f855227b0b97480f4977fc
3ghin5 <yoylecakeisback@gmail.com>
2022-05-13 15:28:16 -0700

    routes

--- main.py
+++ main.py
@@ -11,5 +11,25 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def index():
+    return render_template("about.html")
+
+
+@app.route("/donate")
+def index():
+    return render_template("donate.html")
+
+
+@app.route("/team")
+def index():
+    return render_template("team.html")
+
+
+@app.route("/upcoming")
+def index():
+    return render_template("upcoming.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
